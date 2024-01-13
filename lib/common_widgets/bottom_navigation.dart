0d6d30b2d3b2672d273bbabe9ad040d7fa327fc2
HansFigo <hambasahaya0303@gmail.com>
2024-01-13 15:17:48 +0700

    chore(bottom_navigation.dart): refactor BottomNavigationBar widget to improve code readability and add custom styling feat(bottom_navigation.dart): add support for customizing the background color, border radius, and box shadow of the BottomNavigationBar feat(bottom_navigation.dart): add support for customizing the icon size of the BottomNavigationBar items feat(bottom_navigation.dart): hide labels of the BottomNavigationBar items to create a cleaner UI feat(bottom_navigation.dart): customize the color of the unselected items in the BottomNavigationBar feat(bottom_navigation.dart): remove default elevation and background color of the BottomNavigationBar to create a transparent appearance

--- lib/common_widgets/bottom_navigation.dart
+++ lib/common_widgets/bottom_navigation.dart
@@ -4,6 +4,7 @@ import 'package:tourkuy/pages/home/home_page.dart';
 import 'package:tourkuy/pages/profile_page/profile_page.dart';
 import 'package:tourkuy/pages/search_page/search_page.dart';
 import 'package:tourkuy/providers/bottom_navigation_provider.dart';
+import 'package:tourkuy/styles/style.dart';
 import 'package:tourkuy/utils/goto.dart';
 
 class MyBottomNavigation extends ConsumerWidget {
@@ -12,6 +13,8 @@ class MyBottomNavigation extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final int selectedIndex = ref.watch(bottomNavigationProvider).state;
+
+    const double iconSize = 30.0;
 
     void onItemTapped(int index) {
       ref.read(bottomNavigationProvider).set(index);
@@ -27,24 +30,45 @@ class MyBottomNavigation extends ConsumerWidget {
       }
     }
 
-    return BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.search),
-          label: 'Search',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.account_balance),
-          label: 'Profile',
-        ),
-      ],
-      currentIndex: selectedIndex,
-      selectedItemColor: const Color.fromARGB(255, 6, 196, 158),
-      onTap: onItemTapped,
+    return Container(
+      decoration: BoxDecoration(
+        color: AppStyles.primaryColor, // Ganti warna sesuai kebutuhan
+        borderRadius: const BorderRadius.vertical(
+            top: Radius.circular(32.0)), // Atur border radius
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 7,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, size: iconSize),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search, size: iconSize),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle, size: iconSize),
+            label: 'Profile',
+          ),
+        ],
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: selectedIndex,
+        unselectedItemColor: Color.fromARGB(255, 184, 184, 184),
+        selectedItemColor: Colors.white,
+        onTap: onItemTapped,
+        elevation: 0, // Menghilangkan shadow bawaan dari BottomNavigationBar
+        backgroundColor:
+            Colors.transparent, // Menghilangkan warna latar belakang bawaan
+      ),
     );
   }
 }
